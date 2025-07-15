6448116a150dce2c9fa65cb74b4bd3ca5f026d03
Nahom teshome <nahomteshome708@gmail.com>
2025-07-15 14:58:51 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 load_dotenv()
-BOT_TOKEN = "8030579167:AAF0gTeNvsx6u9ZqCguFcvtuhwLppj6aeOM"
+BOT_TOKEN = "token"
 print("BOT TOKEN:", BOT_TOKEN)
 
 async def start(update, context):
